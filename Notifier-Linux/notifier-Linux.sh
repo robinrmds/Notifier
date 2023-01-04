dc3b73bf73a854c12139b3d996a87bcc8c883c41
robinrmds <119683948+robinrmds@users.noreply.github.com>
2023-01-04 01:52:51 -0300

    Update notifier-Linux.sh
    Bug related to new year fixed

--- Notifier-Linux/notifier-Linux.sh
+++ Notifier-Linux/notifier-Linux.sh
@@ -1,26 +1,31 @@
+#!/bin/bash
+#Autor: Robinson Stürmer
+#Contact: robin.rmds@gmail.com
+
 PREFIX=/opt/notifier
-VALUEIDLASTFILE=$PREFIX/sshd/valueidlast.txt
+VALUEIDLASTFILE=$PREFIX/ssh/valueidlast.txt
 VALUEIDLAST=`tail -1 $VALUEIDLASTFILE`
-TEMPFILE=$PREFIX/sshd/tempSSHD.txt
-NUMLINES=100
+TEMPFILE=$PREFIX/ssh/tempFILE.txt
+NUMLINES=30
 LOGFILE=/var/log/auth.log
 CHAT_ID=
 TOKEN=
 SENDMESSAGE="$PREFIX/lib/telegram.sh $TOKEN $CHAT_ID"
-[ -f "$PREFIX/sshd" ] || mkdir $PREFIX/sshd
-[ -f "$VALUEIDLASTFILE" ] ||echo "1" > "$VALUEIDLASTFILE"
+[ -f "$PREFIX/1-sshd657637586" ] || mkdir $PREFIX/1-sshd657637586
+[ -f "$VALUEIDLASTFILE" ] ||echo `date +%Y%m%d%H%M00` > "$VALUEIDLASTFILE"
 [ -f "$TEMPFILE" ] ||touch "$TEMPFILE"
 
 while [ true ]; do
 	grep sshd $LOGFILE | grep -E "Accepted|error" | tail -$NUMLINES > $TEMPFILE    
 	NUMLINHAS=`wc -l $TEMPFILE |awk '{print $1}'`
 	YEAR=`date +%Y`
+	CURRENTMONTH=`date +%m`
 	i=1
 	while [ $i -le $NUMLINHAS ]; do
 		MONTH=`head -$i $TEMPFILE |tail +$i | awk '{print $1}'`;
 		VALUEIDLAST=`tail -1 $VALUEIDLASTFILE`;
 		DAY=`head -$i $TEMPFILE |tail +$i | awk '{print $2}'`;
-	if [ $DAY -le 10 ]
+	if [ $DAY -lt 10 ]
 	then
 		DAY=0$DAY
 	fi
@@ -63,11 +68,16 @@ while [ true ]; do
 			MONTH=12
 			;;
 		esac
+		
 		VALUEID=$YEAR$MONTH$DAY$HOUR
-		if [ $VALUEID -gt $VALUEIDLAST ]
-			then
+		
+		if [ $VALUEID -gt $VALUEIDLAST ] && [ $MONTH -le $CURRENTMONTH ]
+		then
 			MESSAGE=`head -$i $TEMPFILE |tail +$i | awk '{print $0}'|sed 's/ /%20/g' | sed 's/\[/-/g'| sed 's/\]/-/g'`
-			COMMAND="$SENDMESSAGE $MESSAGE"
+			IP_SRC=`echo $MESSAGE | awk -F "%20" '{print $11}'`
+			MAC_SRC=`arp -a | grep $IP_SRC | awk '{print $4}'`
+			HOST_NAME=`host $IP_SRC |awk '{print $5}'`
+			COMMAND="$SENDMESSAGE SERVER:`cat /etc/hosts | awk '{print $1}' | tail -n 1`%20-%20SSHD%0A%0A$MESSAGE%20|%20MAC%20Adress:$MAC_SRC%20|%20Name:$HOST_NAME"
 			`$COMMAND`
 			echo $VALUEID > $VALUEIDLASTFILE
 		else
